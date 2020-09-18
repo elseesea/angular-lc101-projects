3c4e78ec7e2ad5700e72f1389a4c4259216d8683
Elsee Sea <sub01+lc101@iaonline.net>
2020-09-18 17:15:30 -0500

    Done with 29.6.4

--- lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
+++ lesson2/examples/input-practice/src/app/movie-list/movie-list.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-   movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+	movies = ['Toy Story', 'The Shining', 'Sleepless in Seattle', 'The Martian'];
+	addingBlank = false;
+	addingDuplicate = false;
+	
+	constructor() { }
 
-   constructor() { }
+	ngOnInit() {
+	}
 
-   ngOnInit() {
-   }
+	addMovie (newTitle: string) {
+		this.resetError();
+		newTitle = newTitle.trim();
+		this.addingBlank = this.blankMovie(newTitle);
+		this.addingDuplicate = this.duplicateMovie(newTitle);
+		if(!this.addingDuplicate && !this.addingBlank) {
+			this.movies.push(newTitle);
+		}
+	}
+	
+	blankMovie (newTitle: string) :boolean {
+		if(newTitle.trim()=='') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	duplicateMovie (newTitle: string) :boolean {
+		if(this.movies.includes(newTitle)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	resetError () {
+		this.addingBlank = false;
+		this.addingDuplicate = false;
+	}
 }
